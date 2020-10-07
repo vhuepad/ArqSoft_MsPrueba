a7f08aaccf4eb415e879c772b9b51ba539477eba
ValeHuDU <valeri.hudu@gmail.com>
2020-10-07 15:22:43 -0500

    update

--- Ms_Notificación/src/index.js
+++
@@ -1,9 +0,0 @@
-const app = require( "./app" );
-require( "./database" );
-
-async function main( ) {
-  await app.listen( app.get( "port" ) );
-  console.log( "Server on port", app.get( "port" ) );
-}
-
-main()
