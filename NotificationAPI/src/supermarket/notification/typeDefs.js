a0e1a56004194994dc59353ac2424cf9472963b7
ValeHuDU <valeri.hudu@gmail.com>
2020-10-10 10:41:27 -0500

    test

--- NotificationAPI/src/supermarket/notification/typeDefs.js
+++ NotificationAPI/src/supermarket/notification/typeDefs.js
@@ -1,17 +1,17 @@
 export const notificationTypeDef = `
   type Notification {
       id: String!
-      userId: Int!
-      messageId: Int
-      conversationid: Int
+      userId:Int!
+      messageId: Int!
+      conversationid: Int!
       date: String!
       text: String!
       SenderId: Int!
   }
   input NotificationInput {
       userId: Int!
-      messageIid: Int
-      conversationid: Int
+      messageIid: Int!
+      conversationid: Int!
       date: String!
       text: String!
       SenderId: Int!
@@ -19,12 +19,12 @@ export const notificationTypeDef = `
 
 export const notificationQueries = `
       getAllNotifications(userId: Int!): [Notification]!
-      getNotification(userId: Int!, notId: String!): Notification!
+      getNotification(userId: Int!, notId: String!): [Notification]!
   `;
 
 export const notificationMutations = `
     createNotification(notification: NotificationInput!): Notification!
-    deleteNotification(userId: Int!, notId: String!): Int!
-    deleteAllNotifications(id: Int!): Int!
+    deleteNotification(userId: Int!, notId: String!): String
+    deleteAllNotifications(id: Int!): String
 `;
 
