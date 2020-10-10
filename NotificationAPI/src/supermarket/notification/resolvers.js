d5db3a8083d9c64945289e0390a7fac27807434f
ValeHuDU <valeri.hudu@gmail.com>
2020-10-10 10:21:36 -0500

    test

--- NotificationAPI/src/supermarket/notification/resolvers.js
+++ NotificationAPI/src/supermarket/notification/resolvers.js
@@ -1,4 +1,4 @@
-import { generalRequest, getRequest } from '../../utilities';
+import { generalRequest } from '../../utilities';
 import { url, port, entryPoint } from './server';
 
 const URL = `http://${url}:${port}/${entryPoint}`;
